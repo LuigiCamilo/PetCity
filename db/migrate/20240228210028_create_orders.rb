179effc7934beb4d2eed012da89d1d7fc642341d
Luigi Di Martino <luigic22@gmail.com>
2024-02-28 15:01:02 -0600

    modelos finos

---
+++ db/migrate/20240228210028_create_orders.rb
@@ -0,0 +1,12 @@
+class CreateOrders < ActiveRecord::Migration[7.1]
+  def change
+    create_table :orders do |t|
+      t.integer :quantity
+      t.float :price
+      t.references :user, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
