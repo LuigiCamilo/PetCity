b62f65687f290c3e52c285a07f69330841cae97d
Arianna Gonzalez <ariannargt@gmail.com>
2024-03-04 11:16:39 -0400

    Search funcional

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,14 @@ class Product < ApplicationRecord
   has_many :orders
   has_one_attached :photo
   validates :photo, :title, :description, :stock, :price, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search,
+  against: [ :title, :description, :brand, :pet, :category ],
+  associated_against: {
+    user: [ :first_name, :last_name ]
+  },
+  using: {
+    tsearch: { prefix: true }
+  }
 end
